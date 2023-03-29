570cebfd014fa9dfbca66afd3fdc0c9d1a3fd414
Markiyan333 <markiyan333@gmail.com>
2023-03-30 00:42:14 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Component } from 'react';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    materials: [],
+    searchimg: '',
+    page: 1,
+  };
+
+  incrementPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handlFormSubmit = searchimg => {
+    this.setState({ searchimg, page: 1 });
+  };
+
+  render() {
+    return (
+      <section className={css.App}>
+        <div>
+          <Searchbar handlFormSubmirt={this.handlFormSubmit} />
+          <ImageGallery
+            page={this.state.page}
+            searchimg={this.state.searchimg}
+            incrementPage={this.incrementPage}
+          />
+        </div>
+      </section>
+    );
+  }
+}
